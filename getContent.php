269fd10fcef65d8282701e998d44dc3b4e5675de
Dmitry <55735354+theWolver@users.noreply.github.com>
2019-10-22 14:53:49 +0300

    Create getContent.php

---
+++ getContent.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wolverine
+ * Date: 20.10.2019
+ * Time: 17:40
+ */
+$url = $_GET["my_url"];
+$page = file_get_contents($url);
+echo html_entity_decode($page);
+//echo json_encode($page);
