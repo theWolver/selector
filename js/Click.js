367d32b0a8793ccf91fa4448af11255c300e86ed
Dmitry <55735354+theWolver@users.noreply.github.com>
2019-10-24 15:21:31 +0300

    Update Click.js

--- js/Click.js
+++ js/Click.js
@@ -4,7 +4,7 @@
 
 // обрабатывает клик по кнопке отправки формы и возвращает в iframe содержимое html-файла, URL которого записано в value INPUT'a
 $(function () {
-    $('form').on('click', function (event) {
+    $('#submit').on('click', function (event) {
 
         event.preventDefault();// using this page stop being refreshing
         let value = $('input').val();
@@ -23,6 +23,9 @@ $(function () {
                 my_frame_container.open();
                 my_frame_container.writeln(result);
                 my_frame_container.close();
+            },
+            error: function(response) { // Данные не отправлены
+                //alert("Ошибка. Данные не отправленны.");
             }
         });
 
@@ -32,9 +35,28 @@ $(function () {
 let frame = document.getElementById("my_frame");
 frame.addEventListener("load", iframeClick, false);
 function iframeClick() {
+    //отключаем возможность клика по всем кнопкам установкой атрибута неактивности поля input
+    $('#my_frame').contents().find('input').each(function () {
+        $('#my_frame').contents().find('input').attr('disabled', 'disabled');
+    });
+    //отключаем переходы по гиперссылкам
+    $('#my_frame').contents().find('a').click(function () {
+        event.preventDefault();
+        $(this).css("box-shadow", "3px 3px 33px 6px rgba(250,0,0,1)");
+        sendSelector(event.target.id, event.target.className, event.target.tagName);
+    });
     document.getElementById("my_frame").contentWindow.document.body.onclick = function()
     {
-            sendSelector(event.target.id, event.target.className, event.target.tagName);
+        let msg;
+        if(event.target.id!="")
+            msg = $('#my_frame').contents().find('#'+event.target.id);
+        if(event.target.className)
+            msg = $('#my_frame').contents().find('.'+event.target.className);
+        msg.css("box-shadow", "3px 3px 33px 6px rgba(250,0,0,1)");
+       /* msg.css('border-width', '0');
+        msg.css("border", "3px solid red");*/
+       // msg.html(data); //table_result
+           sendSelector(event.target.id, event.target.className, event.target.tagName);
     }
 }
 
